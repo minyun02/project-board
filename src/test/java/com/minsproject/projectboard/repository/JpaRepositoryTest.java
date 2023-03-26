730a4f4f9e26c42cb153de55bf6c60dff5f8b933
min <myun02302@gmail.com>
2023-03-26 18:31:39 +0900

    #8-게시글 엔티티 작성 완료

---
+++ src/test/java/com/minsproject/projectboard/repository/JpaRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.minsproject.projectboard.repository;
+
+import com.minsproject.projectboard.config.JpaConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("JPA 연결 테스트")
+@Import(JpaConfig.class) //import를 추가하지 않으면 JpaAuditing이 정상적으로 추가되지 않는다.
+@DataJpaTest
+class JpaRepositoryTest {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
+                             @Autowired ArticleCommentRepository articleCommentRepository) {
+        this.articleRepository = articleRepository;
+        this.articleCommentRepository = articleCommentRepository;
+    }
+
+    @DisplayName("select 테스트")
+    @Test
+    void givenTestData_whenSelecting_thenWorksFine() {
+
+    }
+}
