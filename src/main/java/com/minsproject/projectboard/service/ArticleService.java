48690ac40752b7447d45be3a615ab173e43f7d90
min <myun02302@gmail.com>
2023-05-09 22:44:10 +0900

    #24 - 로직 추가

--- src/main/java/com/minsproject/projectboard/service/ArticleService.java
+++ src/main/java/com/minsproject/projectboard/service/ArticleService.java
@@ -6,6 +6,7 @@ import com.minsproject.projectboard.dto.ArticleUpdateDto;
 import com.minsproject.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,19 +21,19 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleDto searchArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
