2b1f40cf8104bcb6034083dd825a2ab343047cc1
min <myun02302@gmail.com>
2023-05-11 23:05:32 +0900

    #24-게시글 작성 기능 구현 1

--- src/main/java/com/minsproject/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/minsproject/projectboard/repository/ArticleRepository.java
@@ -21,7 +21,11 @@ public interface ArticleRepository extends
         QuerydslBinderCustomizer<QArticle>
 {
 
-    Page<Article> findByTitle(String title, Pageable pageable);
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccountContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
 
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
--- src/main/java/com/minsproject/projectboard/service/ArticleService.java
+++ src/main/java/com/minsproject/projectboard/service/ArticleService.java
@@ -1,10 +1,13 @@
 package com.minsproject.projectboard.service;
 
+import com.minsproject.projectboard.domain.Article;
 import com.minsproject.projectboard.domain.type.SearchType;
 import com.minsproject.projectboard.dto.ArticleDto;
 import com.minsproject.projectboard.dto.ArticleUpdateDto;
 import com.minsproject.projectboard.repository.ArticleRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -22,20 +26,44 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
-        return Page.empty();
+        if (searchKeyword == null || searchKeyword.isBlank()) {
+            return articleRepository.findAll(pageable).map(ArticleDto::from);
+        }
+
+        return switch (searchType) {
+            case TITLE -> articleRepository.findByTitleContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case CONTENT -> articleRepository.findByContentContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case ID -> articleRepository.findByUserAccountContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case NICKNAME -> articleRepository.findByUserAccount_NicknameContaining(searchKeyword, pageable).map(ArticleDto::from);
+            case HASHTAG -> articleRepository.findByHashtag("#" + searchKeyword, pageable).map(ArticleDto::from);
+        };
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(Long articleId) {
-        return null;
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
     public void saveArticle(ArticleDto dto) {
+        articleRepository.save(dto.toEntity());
     }
 
     public void updateArticle(ArticleDto dto) {
+        try {
+            Article article = articleRepository.getReferenceById(dto.id());
+            if (dto.title() != null) {article.setTitle(dto.title());}
+            if (dto.content() != null) {article.setContent(dto.content());}
+//        article.setHashtag(dto.hashtag());
+//            articleRepository.save(article);
+        } catch (EntityNotFoundException e) {
+            log.warn("게시글 업데이트 실패. 게시글을 찾을 수 없습니다 - dto: {}", dto);
+        }
+
     }
 
     public void deleteArticle(long articleId) {
+        articleRepository.deleteById(articleId);
     }
 }
